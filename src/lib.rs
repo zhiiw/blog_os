c7aa61788056496018f9a7d2dba758104281ab71
zhiiw <1302593554@qq.com>
2021-09-18 19:58:01 +0800

    page interrupt support

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,24 @@ use core::panic::PanicInfo;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
+pub mod gdt;
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
+    unsafe {
+        interrupts::PICS.lock().initialize();
+    }
+    x86_64::instructions::interrupts::enable();
 }
 pub trait Testable {
     fn run(&self) -> ();
+}
+
+pub fn hlt_loop()->!{
+    loop{
+        x86_64::instructions::hlt();
+    }
 }
 
 impl<T> Testable for T
@@ -41,7 +53,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();         // new
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -66,7 +78,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();         // new
 }
 
 #[cfg(test)]
@@ -74,3 +86,4 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
 }
+
