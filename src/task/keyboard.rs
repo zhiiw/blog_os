3518359d0d519607df577f675b46a379794b70cc
zhiiw <1302593554@qq.com>
2021-09-21 17:33:23 +0800

    future support

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -2,6 +2,9 @@ use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
 use crate::println;
 use core::result::Result::Ok;
+use core::{pin::Pin, task::{Poll, Context}};
+use futures_util::stream::Stream;
+use core::stream::Stream;
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 
@@ -24,5 +27,17 @@ impl ScancodeStream{
     pub fn new() -> Self {
         SCANCODE_QUEUE.try_init_once(||ArrayQueue::new(100)).expect("ScancodeStream::new should only be called once");
         ScancodeStream{_private:()}
+    }
+}
+
+impl Stream for ScancodeStream{
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE.try_get().expect("not initialized");
+        match queue.pop() {
+            Ok(scancode)=>Poll::Ready(Some(scancode)),
+            Err(crossbeam_queue::PopError)=>Poll::Pending,
+        }
     }
 }
